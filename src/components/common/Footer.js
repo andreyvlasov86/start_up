e5d9859fbb5de57b7130fa1a7a1872c28b26f795
Andrey <a.v.vlasov86@gmail.com>
2019-03-03 21:34:23 +0200

    add navigation

--- src/components/common/Footer.js
+++ src/components/common/Footer.js
@@ -12,7 +12,11 @@ export default class Footer extends Component {
 		return (
 			<footer className='container-fluid footer'>
 				<div className='col-lg-2 offset-10 footer-link'>
-					Powered by <a href='https://ideus.biz' target="_blank">iDeus</a>
+					Powered by
+					<a href='https://ideus.biz' target="_blank">
+						<span className='comp-i'>i</span>
+						<span className='comp'>Deus</span>
+					</a>
 				</div>
 			</footer>
 		)
