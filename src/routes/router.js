f407895e05b85ffd8e91a4b64b75bbe17a0aba43
Andrey Vlasov <andrey.vlasov@ideus.biz>
2019-03-07 19:03:22 +0200

    Refactoring

--- src/routes/router.js
+++ src/routes/router.js
@@ -3,22 +3,21 @@ import routerConfig from "./routerConfig";
 import {Route} from 'react-router-dom';
 
 
-
 export const routes = routerConfig.map(route => (
-			<Route
-				key={route.id}
-				path={route.path}
-				component={route.component}
-			/>
-		));
+    <Route
+        key={route.id}
+        path={route.path}
+        component={route.component}
+    />
+));
 
-		routerConfig.map(route => (
-			route.subitems.map(subitem => (
-				routes.push(
-					<Route
-						key={subitem.id}
-						path={subitem.path}
-						component={subitem.component}
-					/>)
-				))
-			));
+routerConfig.map(route => (
+    route.subitems.map(subitem => (
+        routes.push(
+            <Route
+                key={subitem.id}
+                path={subitem.path}
+                component={subitem.component}
+            />)
+    ))
+));
